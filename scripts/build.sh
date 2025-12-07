b54237d6c1075b324054c8f781bb6c02535ebc61
Matthew Ballance <matt.ballance@gmail.com>
2025-12-07 04:17:27 +0000

    Simplify: use OpenROAD's DependencyInstaller for version compatibility

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,59 +6,6 @@ root=$(pwd)
 NPROC=$(nproc)
 
 #********************************************************************
-#* Install required packages
-#********************************************************************
-if test $(uname -s) = "Linux"; then
-    if test -f /etc/lsb-release && grep -q "Ubuntu" /etc/lsb-release; then
-        # Ubuntu
-        apt-get update -y
-        apt-get install -y build-essential cmake git wget flex bison \
-            libffi-dev libreadline-dev \
-            tcl tcl-dev tk tk-dev \
-            python3 python3-dev python3-pip \
-            libpcre3-dev libpcre2-dev \
-            libssl-dev zlib1g-dev \
-            autoconf automake libtool \
-            swig \
-            libqt5charts5-dev qtbase5-dev \
-            pkg-config
-    elif test -f /etc/centos-release && grep -q "CentOS Linux release 7" /etc/centos-release; then
-        # manylinux2014 / CentOS 7
-        yum update -y
-        yum install -y epel-release
-        yum install -y glibc-static wget flex bison jq \
-            cmake3 autoconf automake libtool make gcc gcc-c++ git \
-            zlib-devel tcl tcl-devel tk tk-devel \
-            libffi-devel readline-devel \
-            python3 python3-devel python3-pip \
-            pcre-devel pcre2-devel \
-            openssl-devel
-
-        # Create cmake symlink if cmake3 exists
-        if test -f /usr/bin/cmake3 && test ! -f /usr/bin/cmake; then
-            ln -s /usr/bin/cmake3 /usr/bin/cmake
-        fi
-    else
-        # manylinux_2_28+ / AlmaLinux 8+
-        dnf update -y
-        dnf install -y epel-release
-        dnf install -y glibc-static wget flex bison jq \
-            cmake autoconf automake libtool make gcc gcc-c++ git \
-            zlib-devel tcl tcl-devel tk tk-devel \
-            libffi-devel readline-devel \
-            python3 python3-devel python3-pip \
-            pcre-devel pcre2-devel \
-            openssl-devel qt5-qtbase-devel
-    fi
-
-    if test -z $image; then
-        image=linux
-    fi
-    
-    rls_plat=${image}
-fi
-
-#********************************************************************
 #* Validate environment variables
 #********************************************************************
 if test -z "$openroad_version"; then
@@ -75,157 +22,42 @@ if test "x${BUILD_NUM}" != "x"; then
     rls_version="${rls_version}.${BUILD_NUM}"
 fi
 
+if test -z "$image"; then
+    image=linux
+fi
+rls_plat=${image}
+
 #********************************************************************
 #* Set install prefix
 #********************************************************************
 INSTALL_PREFIX=${root}/release/openroad
 mkdir -p ${INSTALL_PREFIX}
-export PATH=${INSTALL_PREFIX}/bin:$PATH
-export LD_LIBRARY_PATH=${INSTALL_PREFIX}/lib:${INSTALL_PREFIX}/lib64:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=${INSTALL_PREFIX}/lib/pkgconfig:${INSTALL_PREFIX}/lib64/pkgconfig:$PKG_CONFIG_PATH
-export CMAKE_PREFIX_PATH=${INSTALL_PREFIX}:$CMAKE_PREFIX_PATH
 
 #********************************************************************
-#* Build dependencies
-#********************************************************************
-BUILD_DIR=${root}/build_deps
-mkdir -p ${BUILD_DIR}
-cd ${BUILD_DIR}
-
-# Build newer CMake (use pre-built binary to avoid bootstrap issues)
-CMAKE_VERSION="3.28.3"
-if ! test -f ${INSTALL_PREFIX}/bin/cmake; then
-    echo "=== Installing CMake ${CMAKE_VERSION} ==="
-    arch=$(uname -m)
-    wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-${arch}.sh
-    chmod +x cmake-${CMAKE_VERSION}-linux-${arch}.sh
-    ./cmake-${CMAKE_VERSION}-linux-${arch}.sh --skip-license --prefix=${INSTALL_PREFIX}
-    cd ${BUILD_DIR}
-fi
-export PATH=${INSTALL_PREFIX}/bin:$PATH
-
-# Build newer Bison (required by SWIG 4.2+)
-BISON_VERSION="3.8.2"
-if ! test -f ${INSTALL_PREFIX}/bin/bison; then
-    echo "=== Building Bison ${BISON_VERSION} ==="
-    wget https://ftp.gnu.org/gnu/bison/bison-${BISON_VERSION}.tar.gz
-    tar xzf bison-${BISON_VERSION}.tar.gz
-    cd bison-${BISON_VERSION}
-    ./configure --prefix=${INSTALL_PREFIX}
-    make -j${NPROC}
-    make install
-    cd ${BUILD_DIR}
-fi
-export PATH=${INSTALL_PREFIX}/bin:$PATH
-
-# Build Boost
-BOOST_VERSION="1.85.0"
-BOOST_VERSION_UNDERSCORE=${BOOST_VERSION//./_}
-if ! test -f ${INSTALL_PREFIX}/lib/libboost_system.so; then
-    echo "=== Building Boost ${BOOST_VERSION} ==="
-    wget https://archives.boost.io/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_UNDERSCORE}.tar.gz
-    tar xzf boost_${BOOST_VERSION_UNDERSCORE}.tar.gz
-    cd boost_${BOOST_VERSION_UNDERSCORE}
-    ./bootstrap.sh --prefix=${INSTALL_PREFIX}
-    ./b2 install --with-iostreams --with-test --with-serialization --with-system --with-thread --with-filesystem --with-program_options -j${NPROC}
-    cd ${BUILD_DIR}
-fi
-
-# Build spdlog
-SPDLOG_VERSION="1.13.0"
-if ! test -f ${INSTALL_PREFIX}/lib64/libspdlog.a; then
-    echo "=== Building spdlog ${SPDLOG_VERSION} ==="
-    git clone --depth=1 -b v${SPDLOG_VERSION} https://github.com/gabime/spdlog.git
-    cd spdlog
-    cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DSPDLOG_BUILD_EXAMPLE=OFF -B build .
-    cmake --build build -j${NPROC} --target install
-    cd ${BUILD_DIR}
-fi
-
-# Build Eigen
-EIGEN_VERSION="3.4.0"
-if ! test -d ${INSTALL_PREFIX}/include/eigen3; then
-    echo "=== Building Eigen ${EIGEN_VERSION} ==="
-    git clone --depth=1 -b ${EIGEN_VERSION} https://gitlab.com/libeigen/eigen.git
-    cd eigen
-    cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -B build .
-    cmake --build build -j${NPROC} --target install
-    cd ${BUILD_DIR}
-fi
-
-# Build SWIG
-SWIG_VERSION="4.2.1"
-if ! test -f ${INSTALL_PREFIX}/bin/swig; then
-    echo "=== Building SWIG ${SWIG_VERSION} ==="
-    wget https://github.com/swig/swig/archive/refs/tags/v${SWIG_VERSION}.tar.gz -O swig-${SWIG_VERSION}.tar.gz
-    tar xzf swig-${SWIG_VERSION}.tar.gz
-    cd swig-${SWIG_VERSION}
-    ./autogen.sh
-    ./configure --prefix=${INSTALL_PREFIX}
-    make -j${NPROC}
-    make install
-    cd ${BUILD_DIR}
-fi
-
-# Build CUDD
-if ! test -f ${INSTALL_PREFIX}/include/cudd.h; then
-    echo "=== Building CUDD ==="
-    git clone --depth=1 -b 3.0.0 https://github.com/The-OpenROAD-Project/cudd.git
-    cd cudd
-    autoreconf -fi
-    ./configure --prefix=${INSTALL_PREFIX}
-    make -j${NPROC}
-    make install
-    cd ${BUILD_DIR}
-fi
-
-# Build lemon
-LEMON_VERSION="1.3.1"
-if ! test -f ${INSTALL_PREFIX}/include/lemon/config.h; then
-    echo "=== Building Lemon ${LEMON_VERSION} ==="
-    git clone --depth=1 -b ${LEMON_VERSION} https://github.com/The-OpenROAD-Project/lemon-graph.git
-    cd lemon-graph
-    cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -B build .
-    cmake --build build -j${NPROC} --target install
-    cd ${BUILD_DIR}
-fi
-
-# Build or-tools (required by OpenROAD)
-OR_TOOLS_VERSION="9.10"
-if ! test -d ${INSTALL_PREFIX}/include/ortools; then
-    echo "=== Building or-tools ${OR_TOOLS_VERSION} ==="
-    git clone --depth=1 -b v${OR_TOOLS_VERSION} https://github.com/google/or-tools.git
-    cd or-tools
-    cmake -S. -Bbuild \
-        -DBUILD_DEPS=ON \
-        -DBUILD_EXAMPLES=OFF \
-        -DBUILD_SAMPLES=OFF \
-        -DBUILD_TESTING=OFF \
-        -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
-        -DCMAKE_CXX_FLAGS="-w" \
-        -DCMAKE_C_FLAGS="-w"
-    cmake --build build --config Release --target install -j${NPROC}
-    cd ${BUILD_DIR}
-fi
-
-#********************************************************************
-#* Clone and Build OpenROAD
+#* Clone OpenROAD
 #********************************************************************
 cd ${root}
 echo "=== Cloning OpenROAD ${openroad_version} ==="
 git clone --recursive --depth=1 -b ${openroad_version} https://github.com/The-OpenROAD-Project/OpenROAD.git
 cd OpenROAD
 
+#********************************************************************
+#* Install dependencies using OpenROAD's installer
+#********************************************************************
+echo "=== Installing base dependencies ==="
+./etc/DependencyInstaller.sh -base
+
+echo "=== Installing common dependencies ==="
+./etc/DependencyInstaller.sh -common -prefix=${INSTALL_PREFIX}
+
+#********************************************************************
+#* Build OpenROAD
+#********************************************************************
 echo "=== Building OpenROAD ==="
-mkdir -p build
+./etc/Build.sh -cmake="-DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}"
+
+echo "=== Installing OpenROAD ==="
 cd build
-
-cmake .. \
-    -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PREFIX_PATH="${INSTALL_PREFIX}"
-
-make -j${NPROC}
 make install
 
 #********************************************************************
